50522073f3cf666b5520343e564f0e9d89a677b9
Furqan <furqansh.2oo6@gmail.com>
2025-04-13 01:37:28 +0500

    added dll files and created window as test

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Silent_Escape
-A stealth escape game based on SFML and C++ OOP concepts.
+A stealth escape game based on SFML 3.0 and C++ OOP concepts.
+
+# documentation --> SFML 3.0
+https://www.sfml-dev.org/documentation/3.0.0
+
+# tutorials
+https://www.sfml-dev.org/tutorials/3.0
+
+# A little explanation 
+(This is to work in "VS code", not "Microsoft Visual studio")
+
+1. Clone the repository as it is.
+2. While main is open, press ctrl+shift+B for the build process.
+3. Run the silent_escape.exe for the folder in file explorer that was cloned. 
+
+# Contact me for more information!
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-#include <string>
-using namespace std;
+#include <SFML/Window.hpp>
 
 int main()
 {
-    cout << "Hello world" << endl;
-
-    return 0;
+    sf::Window window(sf::VideoMode({1280, 720}), "Silent Escape");
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>() || (event->is<sf::Event::KeyPressed>() && event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Escape))
+            {
+                window.close();
+            }
+            
+        }
+    }
 }
